81ca512ac10e32498b445a4b595dd73050358fdd
Dominick Reinhold <dom.reinhold@gmail.com>
2016-05-22 12:37:44 -0700

    clean up build process

--- tasks/assets.js
+++ tasks/assets.js
@@ -5,28 +5,18 @@ var path = require('path');
 var plugins = require('gulp-load-plugins')();
 var webpack = require('webpack-stream');
 
-function isDevelopment() { return process.env.NODE_ENV === 'development'; }
 function isProduction() { return process.env.NODE_ENV === 'production';}
 
-function javascriptStatic() {
-  return mergeStream([
-    gulp.src(require.resolve(`react/dist/react-with-addons${isDevelopment() ? '' : '.min'}`))
-      .pipe(plugins.rename(`react.js`)),
-    gulp.src(require.resolve(`react-dom/dist/react-dom${isDevelopment() ? '' : '.min'}`))
-      .pipe(plugins.rename(`react-dom.js`))
-  ]);
-}
-
-function javascript(options = {}) {
-  var webpackConfig = Object.assign({}, require('../config/webpack.config')(process.env.NODE_ENV), options);
+function javascript() {
+  var webpackConfig = Object.assign({}, require('../config/webpack.config')(process.env.NODE_ENV), {watch: !isProduction()});
   return gulp.src(['app/components/application.js'])
     .pipe(plugins.plumber())
     .pipe(webpack(webpackConfig));
 }
 
-function sass({watch = false} = {}) {
+function sass() {
   var stream = gulp.src('app/stylesheets/application.scss').pipe(plugins.plumber());
-  if (watch) {
+  if (!isProduction()) {
     stream = stream
       .pipe(plugins.watch('app/stylesheets/**/*.scss'))
       .pipe(plugins.sassGraphAbs([path.join(__dirname, '..', 'app', 'stylesheets')]));
@@ -40,11 +30,7 @@ function sass({watch = false} = {}) {
 }
 
 gulp.task('build', function() {
-  return del(['public/*', '!public/.gitkeep', '!public/index.html', '!public/nginx.conf']).then(function() {
-    return mergeStream(
-      javascriptStatic(),
-      javascript({watch: isDevelopment()}),
-      sass({watch: isDevelopment()})
-    ).pipe(gulp.dest('public'));
+  return del(['public/build/*', '!public/build/.gitkeep']).then(function() {
+    return mergeStream(javascript(), sass()).pipe(gulp.dest('public/build'));
   });
 });
