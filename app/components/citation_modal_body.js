a68aecaf8686702542aaaffa08a5b74c5e395d00
Dominick Reinhold <dom.reinhold@gmail.com>
2016-07-15 12:58:24 -0700

    update phrasing in citation modal

--- app/components/citation_modal_body.js
+++ app/components/citation_modal_body.js
@@ -2,7 +2,7 @@ const React = require('react');
 
 module.exports = () => (
   <div>
-    <p>Thanks for using Mass Spec Solver! If these results contribute to a project that leads to a scientific publication, please acknowledge this fact by citing: </p>
+    <p>Thanks for using Mass Spec Solver! If these results contribute to a project that leads to a scientific publication, a citation would be appreciated:</p>
     <p>Reinhold, D. <i>Mass Spec Solver</i>. <a href="http://www.mass-spec-solver.com">http://www.mass-spec-solver.com</a>{` (accessed ${(new Date()).toDateString().split(' ').slice(1).join(' ')}).`}</p>
   </div>
 );
